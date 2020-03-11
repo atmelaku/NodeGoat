93171d9e0a6446a95acec89015e486242bb6b46d
Melaku <alebachewtegegne2017@yahoo.com>
2020-03-11 12:54:25 -0400

    fixed xss and insecure DOR vulnereblities

--- app/routes/session.js
+++ app/routes/session.js
@@ -107,13 +107,15 @@ function SessionHandler(db) {
             // Fix the problem by regenerating a session in each login
             // by wrapping the below code as a function callback for the method req.session.regenerate()
             // i.e:
-            // `req.session.regenerate(function() {})`
-            req.session.userId = user._id;
-            if (user.isAdmin) {
-              return res.redirect("/benefits");
-            } else {
-              return res.redirect("/dashboard");
-            }
+            req.session.regenerate(function() {
+              req.session.userId = user._id;
+              if (user.isAdmin) {
+                return res.redirect("/benefits");
+              } else {
+                return res.redirect("/dashboard");
+              }
+            })
+
         });
     };
 
@@ -141,7 +143,7 @@ function SessionHandler(db) {
         var FNAME_RE = /^.{1,100}$/;
         var LNAME_RE = /^.{1,100}$/;
         var EMAIL_RE = /^[\S]+@[\S]+\.[\S]+$/;
-        var PASS_RE = /^.{1,20}$/;
+        var PASS_RE = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
         /*
         //Fix for A2-2 - Broken Authentication -  requires stronger password
         //(at least 8 characters with numbers and both lowercase and uppercase letters.)
--- server.js
+++ server.js
@@ -84,14 +84,14 @@ MongoClient.connect(config.db, function(err, db) {
         secret: config.cookieSecret,
         // Both mandatory in Express v4
         saveUninitialized: true,
-        resave: true
+        resave: true,
         /*
         // Fix for A5 - Security MisConfig
         // Use generic cookie name
         key: "sessionId",
         */
 
-        /*
+
         // Fix for A3 - XSS
         // TODO: Add "maxAge"
         cookie: {
@@ -99,7 +99,7 @@ MongoClient.connect(config.db, function(err, db) {
             // Remember to start an HTTPS server to get this working
             // secure: true
         }
-        */
+
 
     }));
 
@@ -133,12 +133,11 @@ MongoClient.connect(config.db, function(err, db) {
 
     // Template system setup
     swig.setDefaults({
-        // Autoescape disabled
-        autoescape: false
-        /*
+
+        root: __dirname + "/app/views",
         // Fix for A3 - XSS, enable auto escaping
         autoescape: true // default value
-        */
+
     });
 
     // Insecure HTTP connection
